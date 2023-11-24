124fb8eb27129ecd5e19c0ad39b1a70483448cf2
Akash <akashshekara1212@gmail.com>
2023-11-24 01:51:22 -0600

    added new introduction

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Beetle</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -13,6 +14,17 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'Nextra Docs Template',
   },
+  head: (
+    <>
+      <iframe
+        src="https://www.chatbase.co/chatbot-iframe/uFcxHlGynfXDst0avJNSn"
+        width="100%"
+        style={{"height": "50px", "width": "50px", "top": "500px", "right": "100px", "position": "fixed", "z-index": "999999"}}
+        frameborder="0"
+        defer
+      ></iframe>
+    </>
+  )
 }
 
 export default config
